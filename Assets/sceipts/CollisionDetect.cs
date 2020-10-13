ab19b42ef781daf15408c78978c08b8d67a576c0
MK Ibn Hossian <49880340+MKRashed@users.noreply.github.com>
2020-10-14 00:52:25 +0600

    unity game for computer graphics

---
+++ Assets/sceipts/CollisionDetect.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollisionDetect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public PlayerMovement movement;
+    public GameManager gameManager;
+    public Scoring scoreOver;
+    public Text gameOver;
+    public float timeT;
+
+    void OnCollisionEnter(Collision hit)
+    {
+        if(hit.collider.tag == "obstracals")
+        {
+            Debug.Log("Player collied with somethings!");
+            gameOver.text = timeT.ToString("Game Over !! Final Time:0 ");
+            movement.enabled = false;
+            scoreOver.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+
+        }
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeT = timeT + Time.deltaTime;
+        
+    }
+}
---
+++ Assets/sceipts/Scoring.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scoring : MonoBehaviour
+{
+    public Transform Player;
+	public Text scoreText;
+    public float pos;
+    public float timeC;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeC = timeC + Time.deltaTime;
+        pos = 234.5f;
+        //scoreText.text = (pos + Player.position.z).ToString("Distance: .00");
+        scoreText.text = timeC.ToString("Time: .00");
+        if(Player.position.z > 238)
+        {
+            scoreText.text = " levelComplet !!";
+        }
+    }
+}
